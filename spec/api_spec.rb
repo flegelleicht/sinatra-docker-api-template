8f456d0762b640118dcd53a5907a5227a0fb3a5a
Erik Hebisch <flegelleicht@gmail.com>
2018-11-14 22:48:16 +0100

    Adds test case for some JSON structure

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -5,11 +5,23 @@ require 'rack/test'
 
 describe "Api" do
   include Rack::Test::Methods
-  def app() Api end
+  def app() 
+    Api 
+  end
+  subject { '{"a":{"b":{"c":1}}}' }
     
   it "should return nothing successfully from root" do
     get '/'
     expect(last_response).to be_ok
     expect(last_response.body).to eq('')
+  end
+  
+  it "should return basic json information" do
+    get '/json'
+    expect(last_response.content_type).to eq('application/json')
+    
+    json = nil
+    expect { json = JSON.parse(last_response.body) }.not_to raise_exception
+    expect(json).to match({"a" => {"d" => 2, "b" => { "c" => 1 }}})
   end
 end
